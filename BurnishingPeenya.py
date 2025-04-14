289faea943cfe9603f1289db3d8abdd8ac82b78f
ManishaDayaala <Manishadayaala@gmail.com>
2025-04-14 17:37:32 +0530

    Update BurnishingPeenya.py

--- BurnishingPeenya.py
+++ BurnishingPeenya.py
@@ -26,7 +26,7 @@ def set_random_seed(seed_value=42):
     tf.random.set_seed(seed_value)
 
 # Define the main folder path
-MAINFOLDER = r"D:\APPdata_burnishingPeenya"
+MAINFOLDER = r"./APPdata_burnishingPeenya"
 
 # Create other paths relative to the main folder
 training_file_path = os.path.join(MAINFOLDER, "Training", "Training_burnishing.xlsx")  # FIXED TRAINING DATA
